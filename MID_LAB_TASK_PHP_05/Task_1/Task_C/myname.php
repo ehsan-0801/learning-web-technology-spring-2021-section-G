76831902da3c2381660edec79100783773ca32e9
ehsan-0801 <ehsanulahmmed@gmail.com>
2021-02-16 12:04:04 +0600

    Task_c01

---
+++ MID_LAB_TASK_PHP_05/Task_1/Task_C/myname.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <title>Name Input field</title>
+</head>
+
+<body>
+    <form method="POST" action="#">
+        <fieldset>
+            <legend>NAME</legend>
+            <input type="text" name="myname" value="<?php if(isset($_POST['myname'])){ echo $_POST['myname']; }?>"> <br>
+            <hr>
+            <input type="submit" name="submit" value="Submit">
+        </fieldset>
+    </form>
+</body>
+
+</html>
