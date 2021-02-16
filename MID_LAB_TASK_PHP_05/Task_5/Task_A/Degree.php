a628b13c937bc37459194334ab6ad168ea28983a
ehsan-0801 <ehsanulahmmed@gmail.com>
2021-02-16 12:18:48 +0600

    task_a05

---
+++ MID_LAB_TASK_PHP_05/Task_5/Task_A/Degree.php
@@ -0,0 +1,17 @@
+<?php
+	if(isset($_POST['submit'])){
+
+		$Degree	=  $_POST['Degree'];
+
+		if($Degree == ""){
+			echo "Null submission...";
+		}else{
+			echo "Success";
+		}
+
+	}else{
+		echo 'invalid request...';
+	}
+
+
+?>
