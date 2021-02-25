d8002ed30c0a98ab858812b3eb63ec9579c13537
ehsan-0801 <ehsanulahmmed@gmail.com>
2021-02-25 23:11:34 +0600

    HomePage added

---
+++ MID_LAB_TASK_PHP_07_Login_System_with_session/HomePage.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Public Home Page</title>
+</head>
+<body>
+    <table border=".05px" style="border-color: black;border-style:solid;">
+        <tr>
+            <td><img src="Images/ImageCompany.png" alt="Company Logo"></td>
+            <td align="Right" width="100%">
+                <a href="Home.php"><b>Home|</b></a>
+                <a href="Login.php"><b>Login|</b></a>
+                <a href="Registration.php"><b>Registration|</b></a>
+                
+            </td>
+
+        </tr>
+        <tr>
+            <td colspan="2" width="100%" height="400px">
+                <h2>Welcome to xCompany</h2>
+
+            </td>
+        </tr>
+        <tr>
+            <td colspan="2" width="100%" height="100px" align="center">
+                <h3>Copyright©2021</h3>
+
+            </td>
+        </tr>
+    </table>
+</body>
+</html>
