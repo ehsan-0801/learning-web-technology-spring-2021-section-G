7fcd565514e8e8176d3eb9f37048239e8f33df89
unknown <ehsanulahmmed@gmail.com>
2021-03-21 00:13:17 +0600

    Delete added

--- user-mgt with DB connection/controller/delete.php
+++ user-mgt with DB connection/controller/delete.php
@@ -2,7 +2,7 @@
     $connection = mysqli_connect('localhost','root', '', 'userDB');
     $id=$_GET['id'];
 
-    $sql="delete from user where ID='$id'";
+    $sql="delete from users where ID='$id'";
 	mysqli_query($connection, $sql);
 
     header('location: ../view/user_list.php');
