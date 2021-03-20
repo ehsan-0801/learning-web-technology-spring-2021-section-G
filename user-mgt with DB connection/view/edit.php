603faf63a6045b68d4d3f39135d9cb7e5804c14f
unknown <ehsanulahmmed@gmail.com>
2021-03-21 00:02:25 +0600

    UserList modified

--- user-mgt with DB connection/view/edit.php
+++ user-mgt with DB connection/view/edit.php
@@ -1,8 +1,12 @@
 <?php
 	$title = "Edit Page";
-	include('header.php');
-
-	echo $_GET['id'];
+	require('header.php');
+	$connection = mysqli_connect('localhost','root', '', 'userDB');
+	$id=$_GET['id'];
+	$sql="select username, password, email from users where ID=$id";
+	$Data=mysqli_query($connection,$sql);
+	$userdata=mysqli_fetch_assoc($Data);
+	$_SESSION['id']=$id;
 ?>
 
 	<form method="post" action="../controller/update.php">
@@ -11,21 +15,21 @@
 			<table>
 				<tr>
 					<td>Username</td>
-					<td><input type="text" name="username" value="alamin"></td>
+					<td><input type="text" name="username" value="<?php echo$userdata['username'] ?>"></td>
 				</tr>
 				<tr>
 					<td>Password</td>
-					<td><input type="password" name="password" value="123"></td>
+					<td><input type="password" name="<?php echo$userdata['password'] ?>" value="123"></td>
 				</tr>
 				
 				<tr>
 					<td>Email</td>
-					<td><input type="email" name="email" value="alamin@aiub.edu"></td>
+					<td><input type="email" name="email" value="<?php echo$userdata['email'] ?>"></td>
 				</tr>
 				<tr>
 					<td></td>
 					<td>
-						<input type="submit" name="signup" value="Update"> 
+						<input type="submit" name="submit" value="Update"> 
 						<a href="user_list.php">Back</a>
 					</td>
 				</tr>
@@ -33,5 +37,5 @@
 		</fieldset>
 	</form>
 <?php
-	include('footer.php');
+	require('footer.php');
 ?>
